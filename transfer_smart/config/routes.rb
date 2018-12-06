57b0eb94bb9a694577fea6c66ce523bcf8ee9ff1
vagosdim <cse32434@cs.uoi.gr>
2018-12-07 00:14:48 +0200

    Designed some basic layout for the application

--- transfer_smart/config/routes.rb
+++ transfer_smart/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/contact', to: 'static_pages#contact'
+  get '/signup',   to: 'users#new'
 end
