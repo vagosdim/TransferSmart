9cca774c7120bf8e685157d72a4c9a593e5f4f7b
vagosdim <cse32434@cs.uoi.gr>
2019-01-09 20:11:42 +0200

    Initialized Page-Specific-Javascript.!

--- transfer_smart/app/assets/javascripts/application.js
+++ transfer_smart/app/assets/javascripts/application.js
@@ -19,36 +19,3 @@
 //= require_tree .
 
 
-
-var response;
-var form_fields;
-var receiving_ammount;
-var sending_ammount;
-$( document ).ready(function() {
-    console.log( "ready!" );
-    form_fields = document.getElementsByClassName("form-control");
-    receiving_ammount = form_fields["exchange_info[receiving_ammount]"];
-    sending_ammount = form_fields["exchange_info[sending_ammount]"];
-
-
-    receiving_ammount.onmousedown = function(){
-    	console.log("READY");
-	  	var node_list_from = document.getElementsByName("exchange_info[currency_from]");
-		var currency_from = node_list_from[0];
-
-		var node_list_to = document.getElementsByName("exchange_info[currency_to]");
-		var currency_to = node_list_to[0];
-
-		$.get('https://openexchangerates.org/api/latest.json?symbols=EUR,GBP&app_id=2c02b1d3c85e4c7d88fbe5dd983d0965', function(data){
-        console.log(data);
-        response = data;
-
-        var from = response["rates"]["EUR"];
-        var to = response["rates"]["GBP"];
-
-        receiving_ammount.value =  (to/from)*sending_ammount.value;
-        
-     });
-  };
-});
-
--- transfer_smart/app/assets/javascripts/exchange_infos.js
+++ transfer_smart/app/assets/javascripts/exchange_infos.js
@@ -1,4 +1,35 @@
-$( document ).on('turbolinks:load', function() {
-	var form_fields = document.getElementsByClassName("form-control");
-	alert("HI");
-}
+var response;
+var form_fields;
+var receiving_ammount;
+var sending_ammount;
+
+$( document ).on ('turbolinks:load',function(){
+	if ($('meta[name=psj').attr('controller') != 'exchange_infos'){
+		return;
+	}
+	console.log( "ready!" );
+    form_fields = document.getElementsByClassName("form-control");
+    receiving_ammount = form_fields["exchange_info[receiving_ammount]"];
+    sending_ammount = form_fields["exchange_info[sending_ammount]"];
+
+
+    receiving_ammount.onmousedown = function(){
+    	console.log("READY");
+	  	var node_list_from = document.getElementsByName("exchange_info[currency_from]");
+		var currency_from = node_list_from[0];
+
+		var node_list_to = document.getElementsByName("exchange_info[currency_to]");
+		var currency_to = node_list_to[0];
+
+		$.get('https://openexchangerates.org/api/latest.json?symbols=EUR,GBP&app_id=2c02b1d3c85e4c7d88fbe5dd983d0965', function(data){
+        console.log(data);
+        response = data;
+
+        var from = response["rates"]["EUR"];
+        var to = response["rates"]["GBP"];
+
+        receiving_ammount.value =  (to/from)*sending_ammount.value;
+        
+     });
+  };
+});
