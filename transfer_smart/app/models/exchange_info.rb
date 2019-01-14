8b63aa39dc447ec5faa3935e3c4fd6f6cf496a22
vagosdim <cse32434@cs.uoi.gr>
2019-01-14 16:27:40 +0200

    Set validations

--- transfer_smart/app/models/exchange_info.rb
+++ transfer_smart/app/models/exchange_info.rb
@@ -1,9 +1,9 @@
 class ExchangeInfo < ApplicationRecord
 	belongs_to :transfer
 
-	#validates :currency_to, presence: true, length: {minimum: 2, maximum: 3}
-	#validates :currency_from, presence: true, length: {minimum: 2, maximum: 3}
-	#validates :sending_ammount, presence: true
-	#validates :receiving_ammount, presence: true
+	validates :currency_to, presence: true, length: {minimum: 2, maximum: 3}
+	validates :currency_from, presence: true, length: {minimum: 2, maximum: 3}
+	validates :sending_ammount, presence: true
+	validates :receiving_ammount, presence: true
     
 end
